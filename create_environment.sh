abfbf143ba7651086d397c18cfc15d86ff053a9a
Gaddy01 <g.irakoze2@alustudent.com>
2024-09-30 14:34:48 -0700

    tak 1

---
+++ create_environment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir submission_reminder_app
+cd submission_reminder_app
+
+mkdir app
+cd app
+touch reminder.sh
+
+mkdir ../modules
+cd ../modules
+touch functions.sh
+
+mkdir ../assets
+cd ../assets
+touch submissions.txt
+
+mkdir ../config
+cd ../config
+touch config.env
+
+touch ../startup.sh
