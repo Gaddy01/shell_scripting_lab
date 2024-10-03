80a7ad323c4c77a2533c40e36cce7f0d47a49bc9
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-03 04:09:30 -0700

    tasks submission

--- submission_reminder_app/app/reminder.sh
+++ submission_reminder_app/app/reminder.sh
@@ -2,7 +2,7 @@
 
 # Source environment variables and helper functions
 source ./config/config.env
-source ./modules/functions.sh
+source ./modules/functions.sh 
 
 # Path to the submissions file
 submissions_file="./assets/submissions.txt"
---
+++ submission_reminder_app/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+source ./app/reminder.sh
