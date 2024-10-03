cc74a48b45d1c6ec8a0adc51a23bae9e374356c0
Gaddy01 <g.irakoze2@alustudent.com>
2024-10-03 06:05:24 -0700

    finally finish

--- create_environment.sh
+++ create_environment.sh
@@ -6,17 +6,22 @@ cd submission_reminder_app
 mkdir app
 cd app
 touch reminder.sh
+mkdir +x reminder.sh
 
 mkdir ../modules
 cd ../modules
 touch functions.sh
+mkdir +x functions.sh
 
 mkdir ../assets
 cd ../assets
 touch submissions.txt
+mkdir +x submissions.txt
 
 mkdir ../config
 cd ../config
 touch config.env
+mkdir +x config.env
 
 touch ../startup.sh
+mkdir +x ../startup.sh
